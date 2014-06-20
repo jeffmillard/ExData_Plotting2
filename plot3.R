37545ce93259f7cff0f8400364c422a3310dbb04
jeffmillard <jmillard@cox.net>
2014-06-19 18:01:41 -0700

    Plot3
    R code + plot

--- plot3.R
+++ plot3.R
@@ -10,14 +10,13 @@
 #	nonnpoint, onroad, offraod) variable,, which of these four 
 #	sources have seen decreases in emissions from 1999 to 2008 for
 #	Baltimire City?  Whcih have seen increases in emissions from
-#	1999-2008? Use the ggplot2 plotting system to make a plot 
-#	answering this question."
+#	1999-2008? 
+#
+# 	Use the ggplot2 plotting system to make a plot answering this
+#	question."
 #
 # Plot is to be saved as png named "plot3.png"
 #
-## http://www.epa.gov/otaq/standards/basicinfo.htm
-## levels(as.factor(SCC$SCC.Level.Three))])
-
 ## ---------------------------------------------------------------------
 plot3 <- function() {
 	
@@ -25,28 +24,42 @@ plot3 <- function() {
 	# all sources and I will later subset location to only Baltimore 
 	# City, Maryland.
 	
-	NEI <- readRDS("summarySCC_PM25.rds")
+	# NEI <- readRDS("summarySCC_PM25.rds")
 
 	# subset to just Baltimore City
 	baltcity.NEI <- NEI[NEI$fips=="24510",]
 	
-	# still use tapply() to calculate the sum by year, but this time 
-	# I pass a list of indices list(year, type). Rock on!
-	
-	sumbySource <- tapply(baltcity.NEI$Emissions, 
+	# use melt(tapply()) to reshape to sum by year for each source
+	sumbySource <- melt(tapply(baltcity.NEI$Emissions, 
 				    list(baltcity.NEI$year, baltcity.NEI$type), 
-				    sum)
+				    sum))
+	# Give the columns nicer names that "Var1" etc
+	names(sumbySource) <- c("Year", "Type", "Emissions")
 	
+	# *** Fun facts - alternative approach is to use aggregate(): ***
+	# aggr.sumbySrc <- aggregate(Emissions ~ year + type, 
+	#					baltcity.NEI, sum)
+	# but melt(tapply()) is way faster
+	#
+	# Example applied to entire NEI dataframe (one test only)
+	#				user  	system 	elapsed 
+	# melt(tapply())		3.395   	0.379   	3.790 
+	# aggregate()		63.077   	1.214  	72.122 
+	# ***           		End of fun facts                      ***
 	
 	# open png graphic device
-	png(filename = "plot2.png",
-	    width = 640, height = 480, units = "px",
-	    bg = "white",  type = "quartz")
+	png(filename = "plot3.png", 
+	    width = 800, height = 400, units = "px",
+	    type = "quartz")
 
-	# base system xy plot
-	plot(names(sumbyYear), sumbyYear, type='b', col="purple",
-	     main="Baltimore City, Maryland Annual PM2.5 Emissions",
-	     pch=15, xlab="Year", ylab="Emissions (tons)")	
+	# ggplot system build up the graph(s)
+	p3 <- ggplot(sumbySource, aes(Year, Emissions))	
+	p3 <- p3 + geom_point(aes(color = Type), size=4) 
+	p3 <- p3 + geom_line(aes(color = Type))
+	p3 <-	p3 + facet_grid(. ~ Type)
+	p3 <- p3 + labs(title = "Pollutant Emissions by Source Type, Baltimore City, MD 1999-2008")
+	p3 <- p3 + labs(y = "Total Annual Emissions (tons)")
+	print(p3)
 	
 	# done with the plot; output the file by closing graphic device
 	dev.off()
